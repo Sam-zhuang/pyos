044f2ac82d14dbc23f59f0e9397d0d07c81572bf
Sam-zhuang <samzhuang666@163.com>
2021-04-17 21:03:58 +0800

    Change

--- pyos/constants.py
+++ pyos/constants.py
@@ -1,1 +1,20 @@
-MOV = 0x0
+""" Commands of pyos CPU. """
+from types import FunctionType as _FunctionType
+
+
+class _Command:
+    """ Decorator of CPU command. """
+    def __init__(self, func):
+        if not isinstance(func, _FunctionType):
+            raise TypeError(f"func argument must be a function, not {type(func).__name__}")
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.func.is_command = True
+        v = self.func()
+        return v
+
+
+@_Command
+def ADD(a, b):
+    v1 = bin(int(str(a)))
