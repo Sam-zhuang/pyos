91d928b8b75bbebda8e53cccc9dec8ae34ff07eb
Sam-zhuang <samzhuang666@163.com>
2021-04-17 20:10:50 +0800

    Initial commit

---
+++ pyos/Memory.py
@@ -0,0 +1,15 @@
+class Memory:
+    def __init__(self, size, unit):
+        pass
+
+    def get(self, address):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __delitem__(self, key):
+        pass
---
+++ pyos/__init__.py
@@ -0,0 +1,4 @@
+# Currently only a framework, the complete document will be gradually completed in the future (all file)
+import CPU
+import Memory
+import constants
