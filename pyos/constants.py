3b1a60794df367e4ccd4168395caccc86af99cff
Sam-zhuang <samzhuang666@163.com>
2021-04-18 11:18:35 +0800

    Add commands: ADD and SUB

--- pyos/constants.py
+++ pyos/constants.py
@@ -1,20 +1,40 @@
 """ Commands of pyos CPU. """
 from types import FunctionType as _FunctionType
+from functools import wraps as _wraps
 
 
 class _Command:
     """ Decorator of CPU command. """
-    def __init__(self, func):
-        if not isinstance(func, _FunctionType):
-            raise TypeError(f"func argument must be a function, not {type(func).__name__}")
+    def __init__(self, number):
+        if not isinstance(number, _FunctionType):
+            raise TypeError(f"func argument must be a function, not {type(number).__name__}")
+        self.number = number
+
+    def __call__(self, func):
         self.func = func
 
-    def __call__(self, *args, **kwargs):
-        self.func.is_command = True
-        v = self.func()
-        return v
+        @_wraps
+        def wrapper(*args, **kwargs):
+            func.is_command = True
+            func.command_number = self.number
+            v = func(args, kwargs)
+            return v
+        return wrapper
+
+    def __repr__(self):
+        return f'command {self.func.__name__}'
 
 
-@_Command
-def ADD(a, b):
-    v1 = bin(int(str(a)))
+@_Command(0x00)
+def _ADD(a, b):
+    """ Command of add. """
+    return a + b
+
+
+@_Command(0x01)
+def _SUB(a, b):
+    return a - b
+
+
+ADD = _ADD
+SUB = _SUB
